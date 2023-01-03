2dab365dcb6e2c516619ecee830fa0192676852b
karanveersinghA2XYY <121312976+karanveersinghA2XYY@users.noreply.github.com>
2023-01-04 02:41:07 +0530

    Update __main__.py

--- NekoRobot/__main__.py
+++ NekoRobot/__main__.py
@@ -170,11 +170,11 @@ HELP_STRINGS = """
 """
 
 GROUP_START_IMG = (
-    "https://te.legra.ph/file/5f272dc498e4cf91faf92.jpg",
-    "https://te.legra.ph/file/9d8190aba9ac3421af488.jpg",
-    "https://te.legra.ph/file/5b1ce336f0c0216224040.jpg",
-    "https://te.legra.ph/file/f1f67047d973f3aea4004.jpg",
-    "https://te.legra.ph/file/c2526c05d65fd2fbfa58b.jpg",
+    "https://te.legra.ph/file/1256133ec5a1f2575ef62.jpg",
+    "https://te.legra.ph/file/8ecc3963e25887c953257.jpg",
+    "https://te.legra.ph/file/4ad0d354bba6fc88775b4.jpg",
+    "https://te.legra.ph/file/321d7b24a3f948b37bcb1.jpg",
+    "https://te.legra.ph/file/86784e3736afdce88e9fc.jpg",
 )
 
 DONATE_STRING = """❂ I'm Free for Everyone ❂"""
